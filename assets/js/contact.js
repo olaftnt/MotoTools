cd128c0f1470932fba2190d917759bdd3764c3c4
olaftnt <olaf.tnt@gmail.com>
2023-06-03 23:18:06 +0200

    TestBuild2

---
+++ assets/js/contact.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    if (window.location.pathname != "/contact.html") {
+      return;
+    }
+
+    $("#SubmitBtn").on("click", function () {
+        var Name = $("#Name").val();
+        var Email = $("#Email").val();
+        var Message = $("#Message").val();
+
+        if(Name.length == 0 || Email.length == 0 || Message.length == 0)
+        {
+            alert("Wypełnij wszystkie pola");
+            return;
+        }
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {if (this.readyState == 4 && this.status == 200) {
+            alert("Dziękujemy za wiadomośc");
+            location.href = "index.html";
+        }};
+        var Url = "https://lxrrv4fjggnmxq3g57jlgcbyge0yuyxc.lambda-url.eu-north-1.on.aws/?Name="+Name+"&Email="+Email+"&Message="+Message
+        xhttp.open("GET",Url,true);
+        xhttp.send();
+    });
+  });
---
+++ assets/js/utils.js
@@ -0,0 +1,43 @@
+var getUrlParameter = function getUrlParameter(sParam) {
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split("&"),
+    sParameterName,
+    i;
+
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split("=");
+
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined
+        ? true
+        : decodeURIComponent(sParameterName[1]);
+    }
+  }
+  return false;
+};
+
+function formatDate(date) {
+  var d = new Date(date),
+      month = '' + (d.getMonth() + 1),
+      day = '' + d.getDate(),
+      year = d.getFullYear();
+
+  if (month.length < 2) 
+      month = '0' + month;
+  if (day.length < 2) 
+      day = '0' + day;
+
+  return [year, month, day].join('-');
+}
+
+function onlyLettersAndNumbers(str) {
+  return /^[A-Za-z0-9]*$/.test(str);
+}
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+function validateVin(vin) {
+  return vin.length == 17 && onlyLettersAndNumbers(vin);
+}
